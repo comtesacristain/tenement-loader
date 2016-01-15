ffa7c231cfc5520732304474a230070aff326cf9
Michael Sexton <mick.sexton@gmail.com>
2016-01-15 17:13:08 +1100

    Renaming of class

--- src/main/java/au/gov/ga/geoportal/TenementMapping.java
+++ src/main/java/au/gov/ga/geoportal/TenementMapping.java
@@ -17,49 +17,59 @@ import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 @SuppressWarnings("unused")
-public class Mapping{
-	
+public class TenementMapping {
 
-	
-	private Map<String, Map<?,?>> mapping;
+	private Map<String, Map<?, ?>> mapping;
 
 	private Document document;
 
-	public Mapping(String filePath) throws SAXException, IOException, ParserConfigurationException {
-		File file = new File(filePath);
-		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-		this.document = documentBuilder.parse(file);
-		this.document.getDocumentElement().normalize();
-		this.mapping = new HashMap<String, Map<?,?>>();
+	public TenementMapping() {
+
+	}
+
+	public TenementMapping(String filePath) throws SAXException, IOException, ParserConfigurationException {
+
+		readMappingFile(filePath);
 	}
 
 	public void readMappingFile(String filePath) throws ParserConfigurationException, SAXException, IOException {
-		
+
+		File file = new File(filePath);
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		document = documentBuilder.parse(file);
+		document.getDocumentElement().normalize();
+		mapping = new HashMap<String, Map<?, ?>>();
 		NodeList stateList = document.getElementsByTagName("state");
+
 		for (int i = 0; i < stateList.getLength(); i++) {
 			if (stateList.item(i).getNodeType() == Node.ELEMENT_NODE) {
 				HashMap<String, String> keyValuePair = new HashMap<>();
-				Element stateElement =  (Element) stateList.item(i);
-			
+				Element stateElement = (Element) stateList.item(i);
+
 				String state = stateElement.getAttribute("name");
+
 				NodeList mappingList = stateElement.getElementsByTagName("mapping");
-				for (int j=0; j < mappingList.getLength(); j++){
+
+				for (int j = 0; j < mappingList.getLength(); j++) {
 					Node mappingNode = mappingList.item(j);
-					String source = mappingNode.getFirstChild().getTextContent();
-					String target = mappingNode.getLastChild().getTextContent();
-					keyValuePair.put(source, target);
+					if (mappingNode.getNodeType() == Node.ELEMENT_NODE) {
+						Element mappingElement = (Element) mappingNode;
+
+						String source = mappingElement.getElementsByTagName("source").item(0).getTextContent();
+						String target = mappingElement.getElementsByTagName("target").item(0).getTextContent();
+
+						keyValuePair.put(source, target);
+					}
 				}
 				mapping.put(state, keyValuePair);
 
-				
 			}
-		
+
 		}
 	}
-	
 
-	public Map<String, Map<?, ?>> getMapping(){
+	public Map<String, Map<?, ?>> getMapping() {
 		return mapping;
 	}
 }
